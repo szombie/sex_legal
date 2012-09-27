63290f2b95aa821de1a7a6eed86bd45cc6960553
Pablo May <pablojesusmay@hotmail.com>
2012-09-27 15:52:25 -0500

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sextantlegal::Application.config.secret_token = '84797ccba17580b6b09465cfab642145d0f64a0e871ce6657d9e53d1dd12bbeea41bc03aba7dcbe7af658cdb0a91e1d75edb1d38d61b1a274ae219cf01d0cb4d'
