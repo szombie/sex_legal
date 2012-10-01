441f802dd6a3c3db59f5a484e0e85f9ec357fe5b
Pablo May <pablojesusmay@hotmail.com>
2012-10-01 15:42:49 -0500

    Home 1.0

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+	def menu(link, action)
+		content_tag(:li, link_to(link, {:controller => "pages", :action => action},:class => li_active(action)))
+	end
+
+	def li_tag(content)
+		content_tag(:li, content)
+	end
+
+	private
+	def li_active(action)
+		action == params[:action]
+	end
 end
