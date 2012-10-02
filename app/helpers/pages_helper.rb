c06eede8d6845e958e7829cf2c9cbb38049f50c0
Pablo May <pablojesusmay@hotmail.com>
2012-10-01 21:08:06 -0500

    estilo de hr borrado

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,10 +3,6 @@ module PagesHelper
 		content_tag(:li, link_to(link, {:controller => "pages", :action => action},:class => li_active(action)))
 	end
 
-	def li_tag(content)
-		content_tag(:li, content)
-	end
-
 	private
 	def li_active(action)
 		action == params[:action]
