23cead28459881626cbd40c63ad61a49b303edab
zombiedevf <zombiedevf@gmail.com>
2012-10-02 14:44:53 -0500

    Se agrego el submenu, si creo una una funcion el el helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,6 +3,10 @@ module PagesHelper
 		content_tag(:li, link_to(link, {:controller => "pages", :action => action},:class => li_active(action)))
 	end
 
+	def menu_footer(link,action)
+		content_tag(:li, link_to(link, {:controller => "pages", :action => action}))
+	end
+	
 	private
 	def li_active(action)
 		action == params[:action]
